b0be802d64cf41378a8cf9dd1765f146c54699db
Aalexdev <aaleex3984@gmail.com>
2021-12-11 11:29:31 +0100

    stb implementation + textureImage class 1/2

---
+++ include/TextureImage.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Device.hpp"
+
+// std
+#include <string>
+
+// libs
+#include <vulkan/vulkan.h>
+
+class TextureImage{
+    public:
+        TextureImage(Device &device, const std::string &filepath);
+        ~TextureImage();
+    
+    private:
+        void createTextureImage(const std::string &filepath);
+        static void createImage(Device &device, uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage &image, VkDeviceMemory& imageMemory);
+
+        Device &device;
+
+        VkImage textureImage;
+        VkDeviceMemory textureImageMemory;
+};
